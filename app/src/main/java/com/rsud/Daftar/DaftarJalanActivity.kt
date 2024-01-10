03bec1fa7c0bead7dcfe4ca8952bae04bf66c91b
Bayyeee <muhammadubai8@gmail.com>
2024-01-10 21:10:34 +0800

    MainActivity V1

---
+++ app/src/main/java/com/rsud/Daftar/DaftarJalanActivity.kt
@@ -0,0 +1,12 @@
+package com.rsud.Daftar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rsud.R
+
+class DaftarJalanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_daftar_jalan)
+    }
+}
