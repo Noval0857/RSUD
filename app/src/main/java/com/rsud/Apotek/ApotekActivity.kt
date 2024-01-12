d584b0aa93ca3a01f4bd39ae0a523182456ba40c
Bayyeee <muhammadubai8@gmail.com>
2024-01-12 11:26:00 +0800

    Desain Activty

---
+++ app/src/main/java/com/rsud/Apotek/ApotekActivity.kt
@@ -0,0 +1,12 @@
+package com.rsud.Apotek
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rsud.R
+
+class ApotekActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_apotek)
+    }
+}
